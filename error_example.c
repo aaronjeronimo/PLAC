3c22c2dbc9b1215a31a2ccab9dc891359f72b5c9
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-06-15 17:52:40 +0200

    Add files via upload

---
+++ error_example.c
@@ -0,0 +1,105 @@
+/* Big command 	*/
+
+// Method and class declaration
+public class ExampleClass {
+
+    // Assign
+    public void setX(int value) {
+        x = 5.99d
+    }
+
+    // Return
+    public double getY() {
+        return y;
+    }
+
+    // Printing
+    public void printName() {
+        out.print("name", name;
+    }
+}
+
+// If and Loops
+public class ControlFlowExample {
+    int a;
+    boolean b;
+
+    private void checkValue(int value) {
+        if (value > 10) {
+            out.print("Value is greater than 10");
+        } else if (value == 10) {
+            out.print("Value is exactly 10");
+        } else {
+            out.print("Value is less than 10")
+        }
+    }
+
+    public void loopExample() {
+        for (int i = 0; i < 5; i = i+5) {
+            out.print("valor");
+        
+
+	// int j;
+        // int j = 0;
+        do 
+            out.print("valor");
+            j = j+1;
+        } while (j 5);
+    }
+}
+
+// Switch
+public class SwitchExample {
+    int day;
+
+    public void printDayOfWeek( {
+        switch (day) {
+            case 1:
+                break;
+            case 2:
+                out.print("Tuesday");
+                break;
+            case :
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+        }
+    }
+}
+
+// Variables and methods
+public class VariableMethodExample {
+    int _num
+    char _letter;
+    boolean _isTrue;
+    double _decimalValue;
+    String _text;
+    char _extra;
+    SwitchExample _test = new SwitchExample();
+
+    public void setValues(int num, char letter, boolean isTrue, double decimalValue, String text) {
+         = num;
+        _letter = letter;
+        _isTrue = isTrue;
+        _decimalValue = decimalValue;
+        _text = text;
+        _test = _test.var;
+    }
+
+    public void printValues) {
+        out.print("Num: ", num);
+        out.print("Letter: ", letter);
+        out.print("Boolean: ", isTrue);
+        out.print("Double: ", decimalValue);
+        out.print("Text: ", text);
+    }
+}
+
+
